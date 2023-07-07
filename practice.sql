804eb6e03733e6e7e4d01b89c32dcb7797e0a8cc
Andrei Chekmasov <andrei.chekmasov@softwire.com>
2023-07-07 11:18:57 +0100

    update practice.sql with table creation code

--- practice.sql
+++ practice.sql
@@ -12,24 +12,30 @@
 -- user_id (key), username (username), password (password) firstname (string), lastname (string), email (email), address (postcode)
 
 
-CREATE TABLE checked_out (
-  id INT PRIMARY KEY SERIAL,
-  checkout_date DATE,
-  due_date DATE,
-  FOREIGN KEY (book_id) REFERENCES books(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
 
-CREATE TABLE books (
-  book_id INT PRIMARY KEY SERIAL,
+CREATE TABLE IF NOT EXISTS books (
+  book_id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   author VARCHAR(255),
   ISBN VARCHAR(255),
   isChecked_out boolean
 );
 
-CREATE TABLE users (
-  user_id INT PRIMARY KEY SERIAL,
-  username VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
   password VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS checked_out (
+  checkout_id SERIAL PRIMARY KEY,
+  checkout_date DATE,
+  due_date DATE,
+  book_id INT,
+  user_id INT,
+  FOREIGN KEY (book_id) REFERENCES books(book_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- selects all tables in DB for output
+SELECT * FROM pg_catalog.pg_tables;
